b30d3c3cdf4c3da4fddc79bca3a8bc8a21dc89fc
Brantlee Griffith <35341280+BrantleeG@users.noreply.github.com>
2018-03-21 11:02:37 -0700

    Update
    Update

--- Projects/Final/script.js
+++ Projects/Final/script.js
@@ -3,3 +3,27 @@
 console.log("reading info");
 
 console.log("working...");
+
+var usaflag = document.getElementById("usaflag");
+var germanyflag = document.getElementById("germanyflag");
+var franceflag = document.getElementById("franceflag");
+var spainflag = document.getElementById("spainflag");
+var japanflag = document.getElementById("japanflag");
+var brazilflag = document.getElementById("brazilflag");
+
+var thumbsupimg = document.getElementById("thumbsupimg");
+var okimg = document.getElementById("okimg");
+var handarmimg = document.getElementById("handarmimg");
+var eyeimg = document.getElementById("eyeimg");
+
+var usa = document.getElementById("usa");
+var germany = document.getElementById("germany");
+var france = document.getElementById("france");
+var spain = document.getElementById("spain");
+var japan = document.getElementById("japan");
+var brazil = document.getElementById("brazil");
+
+var thumbsup = document.getElementById("thumbsup");
+var ok = document.getElementById("ok");
+var handarm = document.getElementById("handarm");
+var eye = document.getElementById("eye");
