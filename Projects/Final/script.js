29053904d61511ed066897b75be3b68c25a02d07
Brantlee Griffith <35341280+BrantleeG@users.noreply.github.com>
2018-03-21 17:53:43 -0700

    Update
    Update

--- Projects/Final/script.js
+++ Projects/Final/script.js
@@ -4,6 +4,8 @@ console.log("reading info");
 
 console.log("working...");
 
+var click=0;
+
 var usaflag = document.getElementById("usaflag");
 var germanyflag = document.getElementById("germanyflag");
 var franceflag = document.getElementById("franceflag");
@@ -27,3 +29,85 @@ var thumbsup = document.getElementById("thumbsup");
 var ok = document.getElementById("ok");
 var handarm = document.getElementById("handarm");
 var eye = document.getElementById("eye");
+
+usa.addEventListener('change', function() {
+  usaflag.style.webkitTransform = 'scale(1.2,1.2)';
+  germanyflag.style.webkitTransform = 'scale(1,1)';
+  franceflag.style.webkitTransform = 'scale(1,1)';
+  spainflag.style.webkitTransform = 'scale(1,1)';
+  japanflag.style.webkitTransform = 'scale(1,1)';
+  brazilflag.style.webkitTransform = 'scale(1,1)';
+})
+
+germany.addEventListener('change', function() {
+  usaflag.style.webkitTransform = 'scale(1,1)';
+  germanyflag.style.webkitTransform = 'scale(1.2,1.2)';
+  franceflag.style.webkitTransform = 'scale(1,1)';
+  spainflag.style.webkitTransform = 'scale(1,1)';
+  japanflag.style.webkitTransform = 'scale(1,1)';
+  brazilflag.style.webkitTransform = 'scale(1,1)';
+})
+
+france.addEventListener('change', function() {
+  usaflag.style.webkitTransform = 'scale(1,1)';
+  germanyflag.style.webkitTransform = 'scale(1,1)';
+  franceflag.style.webkitTransform = 'scale(1.2,1.2)';
+  spainflag.style.webkitTransform = 'scale(1,1)';
+  japanflag.style.webkitTransform = 'scale(1,1)';
+  brazilflag.style.webkitTransform = 'scale(1,1)';
+})
+
+spain.addEventListener('change', function() {
+  usaflag.style.webkitTransform = 'scale(1,1)';
+  germanyflag.style.webkitTransform = 'scale(1,1)';
+  franceflag.style.webkitTransform = 'scale(1,1)';
+  spainflag.style.webkitTransform = 'scale(1.2,1.2)';
+  japanflag.style.webkitTransform = 'scale(1,1)';
+  brazilflag.style.webkitTransform = 'scale(1,1)';
+})
+
+japan.addEventListener('change', function() {
+  usaflag.style.webkitTransform = 'scale(1,1)';
+  germanyflag.style.webkitTransform = 'scale(1,1)';
+  franceflag.style.webkitTransform = 'scale(1,1)';
+  spainflag.style.webkitTransform = 'scale(1,1)';
+  japanflag.style.webkitTransform = 'scale(1.2,1.2)';
+  brazilflag.style.webkitTransform = 'scale(1,1)';
+})
+
+brazil.addEventListener('change', function() {
+  usaflag.style.webkitTransform = 'scale(1,1)';
+  germanyflag.style.webkitTransform = 'scale(1,1)';
+  franceflag.style.webkitTransform = 'scale(1,1)';
+  spainflag.style.webkitTransform = 'scale(1,1)';
+  japanflag.style.webkitTransform = 'scale(1,1)';
+  brazilflag.style.webkitTransform = 'scale(1.2,1.2)';
+})
+
+thumbsup.addEventListener('change', function() {
+  thumbsupimg.style.webkitTransform = 'scale(1.1,1.1)';
+  okimg.style.webkitTransform = 'scale(1,1)';
+  handarmimg.style.webkitTransform = 'scale(1,1)';
+  eyeimg.style.webkitTransform = 'scale(1,1)';
+})
+
+ok.addEventListener('change', function() {
+  thumbsupimg.style.webkitTransform = 'scale(1,1)';
+  okimg.style.webkitTransform = 'scale(1.1,1.1)';
+  handarmimg.style.webkitTransform = 'scale(1,1)';
+  eyeimg.style.webkitTransform = 'scale(1,1)';
+})
+
+handarm.addEventListener('change', function() {
+  thumbsupimg.style.webkitTransform = 'scale(1,1)';
+  okimg.style.webkitTransform = 'scale(1,1)';
+  handarmimg.style.webkitTransform = 'scale(1.1,1.1)';
+  eyeimg.style.webkitTransform = 'scale(1,1)';
+})
+
+eye.addEventListener('change', function() {
+  thumbsupimg.style.webkitTransform = 'scale(1,1)';
+  okimg.style.webkitTransform = 'scale(1,1)';
+  handarmimg.style.webkitTransform = 'scale(1,1)';
+  eyeimg.style.webkitTransform = 'scale(1.1,1.1)';
+})
